c3304a7c35c81f414df2eb61d3c9c3394e1dd0de
KIM JAE YEONG <jaeyeong0809@gmail.com>
2025-08-21 15:50:23 +0900

    작업실 패널 디자인 변경

--- Assets/Scripts/Work_Shop/WorkShopBg.cs
+++ Assets/Scripts/Work_Shop/WorkShopBg.cs
@@ -20,6 +20,11 @@ public class WorkShopBg : MonoBehaviour
         gameManager.OnBgTimeChanged += NowtimeChanged;
     }
 
+    private void OnDisable()
+    {
+        gameManager.OnBgTimeChanged -= NowtimeChanged;
+    }
+
     private void NowtimeChanged(BgType nowtime)
     {
         if(nowtime == BgType.DAY)
