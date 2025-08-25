1adcf0bb25d98bd890b77ad340f7a76235aee409
CHEON JIYEON <milk0618@kw.ac.kr>
2025-08-25 15:25:18 +0900

    엥

--- Assets/Resources/Quest/QuestManager.cs
+++ Assets/Resources/Quest/QuestManager.cs
@@ -109,6 +109,16 @@ public class QuestManager : MonoBehaviour
             StartNewDay();
         }
 
+        gameManager.Add_InventoryItem("은하꿈실", 1);
+        gameManager.Add_InventoryItem("오로라빛이불", 1);
+        gameManager.Add_InventoryItem("햇빛운무솜", 1);
+        gameManager.Add_InventoryItem("몽환의꽃잎", 1);
+        gameManager.Add_InventoryItem("햇빛운무솜", 1);
+        gameManager.Add_InventoryItem("몽환의꽃잎", 1);
+        gameManager.Add_InventoryItem("청야달조각", 2);
+        Debug.Log($"[INV] 청야달조각: {gameManager.Count_InventoryItem("청야달조각")}");
+        Debug.Log($"[INV] 오로라빛이불: {gameManager.Count_InventoryItem("오로라빛이불")}");
+
     }
 
     private void WipeAllQuestRows()
