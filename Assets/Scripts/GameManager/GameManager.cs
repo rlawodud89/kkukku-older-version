3ec61a7ceb77b602e84d50a0c4913f77fefcdec2
KIM JAE YEONG <jaeyeong0809@gmail.com>
2025-08-27 20:42:52 +0900

    Addressable 빌드 세팅

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -1,7 +1,7 @@
-
 using UnityEngine;
 using System.Collections.Generic;
 using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 using System.Linq;
 using System;
 using UnityEngine.SceneManagement;
